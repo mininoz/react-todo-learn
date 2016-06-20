0313289913dc9a9661bc6a3d33a34652683b645f
Min S <Min S>
2016-06-20 16:59:31 +0700

    add delete todo list

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 
 export default class ToDoForm extends Component {
+
+  renderTodo() {
+    return this.props.todos.map( (todo, index) => {
+      return (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={this.props.onDelete.bind(null, index)}> del </button>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
         <h1> Todos </h1>
         <ul>
-          <li> list </li>
-          <li> ddd </li>
-          <li> bbb </li>
-          <li> list </li>
+          { this.renderTodo() }
         </ul>
       </div>
     )
--- src/main.js
+++ src/main.js
@@ -4,12 +4,39 @@ import { render } from 'react-dom'
 import TodoForm from './components/todo-form'
 import TodoList from './components/todo-list'
 
+const initialTodos = [
+  { id: 1, text: 'Go to School', done: false},
+  { id: 2, text: 'Play football', done: false},
+  { id: 3, text: 'Drink Beer', done: false},
+]
+
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: initialTodos
+    }
+
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+
+  handleDelete(index) {
+    console.log('delete', index)
+    let newTodos = this.state.todos
+    newTodos.splice(index,1)
+
+    this.setState({ todos: newTodos })
+  }
+
   render() {
     return (
       <div>
         <TodoForm />
-        <TodoList />
+        <TodoList
+          todos={this.state.todos}
+          onDelete={this.handleDelete}
+        />
       </div>
     )
   }
